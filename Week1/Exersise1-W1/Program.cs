e85b638e208338b0388487c73778e817d70add46
Your Name <ntri1104@gmail.com>
2021-12-20 10:10:24 +0700

    have to has

--- Week1/Exersise1-W1/Program.cs
+++ Week1/Exersise1-W1/Program.cs
@@ -15,15 +15,15 @@ else
 {
     if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
     {
-        Console.WriteLine($"This month have : {day31} days");
+        Console.WriteLine($"This month has : {day31} days");
     }
     else if (month == 4 || month == 6 || month == 9 || month == 11) 
     {
-        Console.WriteLine($"This month have : {day30} days");
+        Console.WriteLine($"This month has : {day30} days");
     }
     else if (month == 2 && year % 4 == 0)
     {
-        Console.WriteLine($"This month have : {day29} days");
+        Console.WriteLine($"This month has : {day29} days");
     }
     else 
     {
