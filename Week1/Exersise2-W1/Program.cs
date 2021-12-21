5877463f75baeacbeb7281feed9da0725aff250c
Your Name <ntri1104@gmail.com>
2021-12-21 14:34:32 +0700

    add exersise 2

---
+++ Week1/Exersise2-W1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+int i;
+int j;
+for (i = 1; i <= 10; i++)
+{
+    for (j = 1; j <=9; j++) 
+    {
+        int ji = j * i;
+        Console.Write($"|{j} x {i} = {ji}\t");
+        
+    }
+Console.WriteLine();
+}
